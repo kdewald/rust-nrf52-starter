1fc9381fd8818fd172fc248d5360a10f37498cff
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2022-02-19 22:43:46 -0800

    LED blinks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
 use panic_probe as _;
-use rtt_target::{rtt_init_print, rprintln};
+use rtt_target::{rprintln, rtt_init_print};
+
+use nrf52840_hal as hal;
+use nrf52840_hal::gpio::Level;
+use nrf52840_hal::prelude::InputPin;
+use nrf52840_hal::prelude::OutputPin;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!(); // You may prefer to initialize another way    
+    rtt_init_print!();
+
+    let p = hal::pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let button = port0.p0_11.into_pullup_input();
+    let mut led = port0.p0_13.into_push_pull_output(Level::Low);
+
     rprintln!("Hello, world!");
-    loop { asm::bkpt() }
+    loop {
+        if button.is_high().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+    }
 }
